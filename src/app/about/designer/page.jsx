03c924aad01c3f7635d0d766beb167a6b1e97568
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-16 16:08:56 +0600

    Create pages, routing in Next.js, and navigation in Next.js

---
+++ src/app/about/designer/page.jsx
@@ -0,0 +1,9 @@
+const Designer = () => {
+    return (
+        <div>
+            <h1>colorful designer</h1>
+        </div>
+    );
+};
+
+export default Designer;
---
+++ src/app/about/developers/page.jsx
@@ -0,0 +1,11 @@
+
+
+const DevelopersPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">This is developers page</h1>
+        </div>
+    );
+};
+
+export default DevelopersPage;
