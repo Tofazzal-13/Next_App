cfee95f381ddc99503ed6d9e431888d756143513
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-16 20:52:05 +0600

    Creating Multiple Layouts in Next.js

---
+++ src/app/dashboard/profile/page.jsx
@@ -0,0 +1,10 @@
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <p>This is my Profile page</p>
+        </div>
+    );
+};
+
+export default ProfilePage;
---
+++ src/app/dashboard/revenue/page.jsx
@@ -0,0 +1,10 @@
+
+const RevenuePage = () => {
+    return (
+        <div>
+            <p>Money is comming soon</p>
+        </div>
+    );
+};
+
+export default RevenuePage;
