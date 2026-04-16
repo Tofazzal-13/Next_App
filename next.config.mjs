4253bfa4c9d766d325eb1c901367638e0b300b51
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-16 22:09:13 +0600

    image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/BK6XLyCj/**',
+        search: '',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
+
+//https://i.ibb.co.com/BK6XLyCj/skysports-kane-williamson-new-zealand-7069504.jpg
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
 
 const AboutPage = () => {
     return (
         <div>
             <h2>This is About page</h2>
+            <Image
+                loading="eager"
+                src="https://i.ibb.co.com/BK6XLyCj/skysports-kane-williamson-new-zealand-7069504.jpg"
+                alt="don pic"
+                width="500"
+                height="300"
+            ></Image>
+            <Image
+                loading="eager"
+                src="/don.jpg"
+                alt="don pic"
+                width="500"
+                height="300"
+
+            ></Image>
         </div>
     );
 };
