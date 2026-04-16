e94cb37738fc483776488ccdc4211104605f5e47
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-16 21:13:52 +0600

    (Recap) Dynamic routing and data loading in NextJS

---
+++ src/app/users/[userId]/page.jsx
@@ -0,0 +1,15 @@
+import { useId } from "react";
+
+const UsersDetailsPage =async ({ params }) => {
+    const {userId} = await params;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    const user = await res.json()
+    return (
+        <div>
+            <p>{user.email}</p>
+            <p>{user.phone}</p>
+        </div>
+    );
+};
+
+export default UsersDetailsPage;
